d6809c82a59add919640fc3b0c7b25cc6b8f409b
Xiao ChenYu <apple.iiiyu@gmail.com>
2020-12-28 12:22:10 +1300

    first styles commit

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
-//Styles
+// Styles
+// eslint-disable-next-line import/extensions
 import styles from './app/styles';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>asdfasdfasdf</Text>
-      <Text>Open up App.tsx to start working on your 11ccc222! </Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerHistory} />
+      <View style={styles.containerOutput}>
+        <View style={styles.placeHolderOutput}>
+          <Text style={styles.txtDefalut}>OUTPUT HERE</Text>
+        </View>
+      </View>
+      <View style={styles.containerButtons} />
     </View>
   );
 }
--- app/styles.tsx
+++ app/styles.tsx
@@ -2,10 +2,31 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    flex: 1,
+    flexDirection: 'column',
+  },
+  containerButtons: {
+    backgroundColor: '#bdc3c7',
+    flex: 0.5,
+  },
+  containerHistory: {
+    backgroundColor: 'red',
+    flex: 0.3,
+  },
+  containerOutput: {
+    flex: 0.2,
+  },
+  placeHolderOutput: {
+    alignItems: 'flex-end',
+    backgroundColor: 'green',
     flex: 1,
     justifyContent: 'center',
+    paddingRight: 20,
+  },
+  txtDefalut: {
+    color: '#fff',
+    fontFamily: 'Helvetica-Light',
+    fontSize: 30,
   },
 });
 
