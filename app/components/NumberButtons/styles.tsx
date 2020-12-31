1fdc6c1366111a373f7d129acb83a524e0316743
ChenYu Xiao <apple.iiiyu@gmail.com>
2020-12-31 16:15:32 +1300

    fix typo

--- app/components/NumberButtons/styles.tsx
+++ app/components/NumberButtons/styles.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
   },
-  txtDefalut: {
+  txtDefault: {
     color: '#000',
     fontFamily: 'Helvetica-Light',
     fontSize: 20,
--- app/styles.tsx
+++ app/styles.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingRight: 20,
   },
-  txtDefalut: {
+  txtDefault: {
     color: '#fff',
     fontFamily: 'Helvetica-Light',
     fontSize: 30,
